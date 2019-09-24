ffcc09ddf5958dfb0b0a3cd13eb6e05e409761ad
Omulosi <mulongojohnpaul@gmail.com>
2019-09-24 20:56:13 +0300

    Create user home page: Displays a list of all students

--- international-ssw/src/components/UserHomePage.js
+++ international-ssw/src/components/UserHomePage.js
@@ -1,3 +1,28 @@
 //Protected page This is a dummy protected page for now that will render a list of student, 
 //have an add student button, and also have a nav bar
 
+import React from 'react';
+import { ProtectedNavBar } from './ProtectedNavBar';
+import { StudentsTable } from './StudentsTable/StudentsTable';
+import Pagination from './Pagination.js';
+
+export const UserHomePage = () => {
+
+  return (
+    <section className="home-page">
+      <ProtectedNavBar />
+      <div className="container home-page-content">
+        <div className="home-page-search">
+          <input type="text" placeholder="Search student" name='query'/>
+        </div>
+
+        <div className="student-list">
+          <StudentsTable />
+        </div>
+
+        <Pagination />
+      </div>
+    </section>
+  )
+}
+
