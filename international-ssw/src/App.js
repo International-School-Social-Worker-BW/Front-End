754ac54bb69c6982a76fcc69ea2d62569ec06b44
Omulosi <mulongojohnpaul@gmail.com>
2019-09-24 14:45:18 +0300

    Update navbar

--- international-ssw/src/App.js
+++ international-ssw/src/App.js
@@ -4,17 +4,22 @@ import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute.js';
 
-// import Login from './components/Login.js';
-// import Registration from './components/Registration.js';
+import Login from './components/Login.js';
+import Registration from './components/Registration.js';
 import UserHomePage from './components/UserHomePage.js';
 import Home from './components/Home.js';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       
       <Switch>
           <Route exact path='/' component={Home} />
+          <Route  path='/about' component={Home} />
+          <Route path='/signin' component={Login} />
+          <Route path='/signup' component={Registration} />
           <PrivateRoute path='/protected' component={UserHomePage} />
       </Switch>
     
